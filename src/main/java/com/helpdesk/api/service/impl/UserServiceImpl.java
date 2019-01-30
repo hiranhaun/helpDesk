d46b4b2b0d83fa42c055c2cc6a08ddeffd26b76d
hiran.filho <hiran.filho@resourceit.com>
2019-01-30 15:10:06 -0300

    Teste

---
+++ src/main/java/com/helpdesk/api/service/impl/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package com.helpdesk.api.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.helpdesk.api.entity.User;
+import com.helpdesk.api.repository.UserRepository;
+import com.helpdesk.api.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public User findByEmail(String email) {
+		return this.userRepository.findByEmail(email);
+	}
+
+	@Override
+	public User createOrUpdate(User user) {
+		return this.userRepository.save(user);
+	}
+
+	@Override
+	public Optional<User> findById(String id) {
+		return this.userRepository.findById(id);	
+	}
+
+	@Override
+	public void delete(String id) {
+		this.userRepository.deleteById(id);
+	}
+
+	@Override
+	public Page<User> findAll(int page, int count) {
+		PageRequest pageRequest =  PageRequest.of(page,count);
+		return userRepository.findAll(pageRequest);
+	}
+
+}
