1226dbdbb996af3a39b2e0ecd8e45223a0f57855
hiran.filho <hiran.filho@resourceit.com>
2019-02-04 15:57:15 -0300

    java

--- src/main/java/com/helpdesk/api/filter/SimpleCORSFilter.java
+++ src/main/java/com/helpdesk/api/filter/SimpleCORSFilter.java
@@ -32,9 +32,9 @@ public class SimpleCORSFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) resp;
 		HttpServletRequest request = (HttpServletRequest) req;
-		response.setHeader("Acess-Control-Allow-Origin", "*");
-		response.setHeader("Acess-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-		response.setHeader("Acess-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
 		
 		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
